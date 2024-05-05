115b0eb3885b8e8c316fe05b549263892cc878f5
ichichavez <isidrochavez429@gmail.com>
2024-05-05 10:37:03 -0400

    Initial Commit

---
+++ Cryptocurrency/CryptocurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptocurrencyApp.swift
+//  Cryptocurrency
+//
+//  Created by Isidro Chávez on 2024-05-05.
+//
+
+import SwiftUI
+
+@main
+struct CryptocurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
